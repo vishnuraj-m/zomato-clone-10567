e31062a6bb1ebc1b9b5d28b8e782dd72325e99f4
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-22 12:27:58 +0530

    server configured and started

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const zomato = express();
+const PORT = 4000;
+
+zomato.use(express.json());
+
+zomato.get("/", (req, res) => {
+    res.json({
+        message: "Server is running",
+    });
+});
+zomato.listen(PORT, () => {
+    console.log("server is running !!!");
+});
